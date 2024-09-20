f8b0d9384147dd539c90316bf480f7a6798d8469
Xiaoze <121001647+sos-xiaobai@users.noreply.github.com>
2024-09-21 01:47:21 +0800

    第一次作业捏

--- main.cpp
+++ main.cpp
@@ -4,4 +4,21 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    int sorted[3] = {a, b, c};
+    //冒泡排序
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = i + 1; j < 3; j++)
+        {
+            if (sorted[i] > sorted[j])
+            {
+                int temp = sorted[i];
+                sorted[i] = sorted[j];
+                sorted[j] = temp;
+            }
+        }
+    }
+    result.min = sorted[0];
+    result.mid = sorted[1];
+    result.max = sorted[2];
 }
